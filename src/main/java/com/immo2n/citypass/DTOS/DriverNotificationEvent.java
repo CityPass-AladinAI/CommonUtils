00793dacc8acfd8afcea6146c84a4e909c2b5b6f
Ferdous-M <mahzabinferdous000@gmail.com>
2026-01-05 17:29:16 +0600

    v1.3

--- src/main/java/com/immo2n/citypass/DTOS/DriverNotificationEvent.java
+++ src/main/java/com/immo2n/citypass/DTOS/DriverNotificationEvent.java
@@ -1,6 +1,6 @@
 package com.immo2n.citypass.DTOS;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.Instant;
@@ -13,6 +13,5 @@ public class DriverNotificationEvent {
     private double pickupLat;
     private double pickupLng;
     private Instant offerExpiryTime;
-
-
 }
+
