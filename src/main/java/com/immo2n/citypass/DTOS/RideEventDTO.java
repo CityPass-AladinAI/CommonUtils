375a6c884fc08e2910f9a9d6e6e0fe5b3cfb7bb7
Ferdous-M <mahzabinferdous000@gmail.com>
2026-01-08 11:50:30 +0600

    Prepare release

--- src/main/java/com/immo2n/citypass/DTOS/RideEventDTO.java
+++ src/main/java/com/immo2n/citypass/DTOS/RideEventDTO.java
@@ -1,22 +1,27 @@
 package com.immo2n.citypass.DTOS;
 
 import lombok.Data;
+import java.time.Instant;
 
 @Data
 public class RideEventDTO {
+
     private String rideId;
     private String driverId;
+
     private double pickupLat;
     private double pickupLng;
+
     private Double destinationLat;
     private Double destinationLng;
+
     private String preferences;
     private long timestamp;
 
     private VehicleType vehicleType;
     private Instant offerExpiryTime;
 
-    public enum VehicleType {   // PascalCase
+    public enum VehicleType {
         BIKE,
         BUS,
         CNG,
